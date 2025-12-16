647f6e6ee1a03fb69c45b648bd4508b08b5b3645
LegitOscar <oscarboerjesen@gmail.com>
2025-12-16 14:51:14 +0100

    Small fixes to security

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import LogIn from './components/Login';
-import LoggedIn from './components/Loggedin';
-import facade from './apiFacade';
-import { useState } from "react"
+import LogIn from "./components/LogIn";
+import LoggedIn from "./components/LoggedIn";
+import facade from "./apiFacade";
+import { useState } from "react";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
-  const logout = () => { facade.logout()
-setLoggedIn(false) } 
-  const login = (user, pass) => {facade.login(user,pass)
-.then(res =>setLoggedIn(true));} 
+  const logout = () => {
+    facade.logout();
+    setLoggedIn(false);
+  };
+  const login = (user, pass) => {
+    facade.login(user, pass).then((res) => setLoggedIn(true));
+  };
 
   return (
     <div>
-      {!loggedIn ? (<LogIn login={login} />) :
-        (<div>
+      {!loggedIn ? (
+        <LogIn login={login} />
+      ) : (
+        <div>
           <LoggedIn loggedIn={loggedIn} />
           <button onClick={logout}>Logout</button>
-        </div>)}
+        </div>
+      )}
     </div>
-  )
+  );
 }
-export default App
+export default App;
--- src/components/LoggedIn.jsx
+++ src/components/LoggedIn.jsx
@@ -1,16 +1,19 @@
-import { useState, useEffect } from 'react'
-import facade from '../apiFacade'
+import { useState, useEffect } from "react";
+import facade from "../apiFacade";
 
-function LoggedIn( { loggedIn }) {
-  const [dataFromServer, setDataFromServer] = useState("Loading...")
+function LoggedIn({ loggedIn }) {
+  const [dataFromServer, setDataFromServer] = useState("Loading...");
 
-  useEffect(() => { facade.fetchData().then(data=> setDataFromServer(data));}, [])
+  useEffect(() => {
+    facade.fetchData().then((data) => setDataFromServer(data));
+  }, []);
 
-  return (
-    <div>
-      <h2>Data Received from server</h2>
-      <h3>{dataFromServer}</h3>
-    </div>
-  )
+  if (!loggedIn)
+    return (
+      <div>
+        <h2>Data Received from server</h2>
+        <h3>{dataFromServer}</h3>
+      </div>
+    );
 }
-export default LoggedIn
+export default LoggedIn;
