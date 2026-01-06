b0cc8132e1d845b02cb79b2b13f6520a3b551bdc
Alex Brøndbjerg <alex.brondbjerg@persgroep.net>
2026-01-06 11:27:50 +0100

    Layout

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Header from "/Users/alexbrondbjerg/DndhelperFrontend/FrontendDNDHelper/src/components/layout/Header.jsx";
+import Header from "/src/components/layout/Header.jsx";
 
 export default function MainLayout() {
   return (
